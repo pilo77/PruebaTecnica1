4e672793b5dcc0d8f163f88fdfed8a1be61fee3a
pilo77 <cavillamil-2022a@corhuila.edu.co>
2026-02-13 11:40:57 -0500

    feat: add ClienteRepository interface

--- PruebaTecnica1/banco/src/main/java/com/pruebatecnica/banco/repository/ClienteRepository.java
+++ PruebaTecnica1/banco/src/main/java/com/pruebatecnica/banco/repository/ClienteRepository.java
@@ -6,8 +6,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+
     Optional<Cliente> findByNumeroIdentificacion(String numeroIdentificacion);
 
     Optional<Cliente> findByCorreoElectronico(String correoElectronico);
+
+    boolean existsByNumeroIdentificacion(String numeroIdentificacion);
 
 }
